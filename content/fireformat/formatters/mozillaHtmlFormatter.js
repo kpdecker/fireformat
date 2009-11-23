629ea15c5050386302ca94398bdccc9f8d6dfb7c
kpdecker <kpdecker@gmail.com>
2009-11-23 08:00:27 +0000

    Native Mozilla HTML formatter.
    git-svn-id: http://fbug.googlecode.com/svn/extensions/fireformat/branches/fireformat1.0@4931 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ content/fireformat/formatters/mozillaHtmlFormatter.js
@@ -0,0 +1,26 @@
+/* See license.txt for terms of usage */
+FBL.ns(function() {
+  const Cc = Components.classes;
+  const Ci = Components.interfaces;
+  const nsIDocumentEncoder = Ci.nsIDocumentEncoder;
+  const EncoderService = Cc["@mozilla.org/layout/documentEncoder;1?type=text/html"];
+
+  var i18n = document.getElementById("strings_fireformat");
+
+  var Format = {};
+  Components.utils.import("resource://fireformat/formatters.jsm", Format);
+
+  Format.Formatters.registerFormatter({
+    type: "HTML",
+    name: "com.incaseofstairs.mozillaHTMLFormatter",
+    display: i18n.getString("MozillaHTMLFormatter"),
+    format: function(object) {
+      // The Document encoder handles all of the heavy lifting here: encoding and line break conversion
+      var serializer = EncoderService.createInstance(nsIDocumentEncoder);
+      serializer.init(document, "text/html", nsIDocumentEncoder.OutputPreformatted);
+      serializer.setCharset("UTF-8");
+      serializer.setNode(object);
+      return serializer.encodeToString().replace(/\r\n/mg, '\n');
+    }
+  });
+});
