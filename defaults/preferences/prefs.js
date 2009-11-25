2634d7d587bd18959e5606648299e0956e4d45b3
kpdecker <kpdecker@gmail.com>
2009-11-25 08:29:16 +0000

    License
    git-svn-id: http://fbug.googlecode.com/svn/extensions/fireformat/branches/fireformat1.0@4975 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,2 +1,3 @@
+/* See license.txt for terms of usage */
 pref("extensions.firebug.fireformat.htmlFormatter", "com.incaseofstairs.fireformatHTMLFormatter");
 pref("extensions.firebug.fireformat.cssFormatter", "com.incaseofstairs.fireformatCSSFormatter");
