c98ae86e9152e929f22efd3bb8506fc5590bca48
kpdecker <kpdecker@gmail.com>
2009-11-26 21:29:16 +0000

    Test pref handler
    git-svn-id: http://fbug.googlecode.com/svn/extensions/fireformat/branches/fireformat1.0@5014 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ test/FBTestFireformat.js
@@ -0,0 +1,21 @@
+/* See license.txt for terms of usage */
+var FBTestFireformat = {
+    PrefHandler: function(prefs) {
+      var Firebug = FW.Firebug;
+      var original = [];
+      for (var i = 0; i < prefs.length; i++) {
+        original.push(Firebug.getPref(Firebug.prefDomain, prefs[i]));
+      }
+      
+      return {
+        setPrefs: function() {
+          for (var i = 0; i < prefs.length; i++) {
+            Firebug.setPref(Firebug.prefDomain, prefs[i], arguments[i]);
+          }
+        },
+        reset: function() {
+          this.setPrefs.apply(this, original);
+        }
+      };
+    }
+};
--- test/formatter/cssFormatter/selectorTextTest.js
+++ test/formatter/cssFormatter/selectorTextTest.js
@@ -1,8 +1,15 @@
 /* See license.txt for terms of usage */
 function runTest() {
-  var Firebug = FW.Firebug;
   var Format = {};
   Components.utils.import("resource://fireformat/formatters.jsm", Format);
+
+  FBTest.loadScript("FBTestFireformat.js", this);
+  var prefs = new FBTestFireformat.PrefHandler([
+       "fireformatCssFormatter.wrapSize",
+       "fireformatCssFormatter.selectorText.selectorsPerLine",
+       "fireformatCssFormatter.selectorText.spaceCount",
+       "fireformatCssFormatter.selectorText.indentLevel"
+  ]);
 
   function getStyle(selectorText) {
     return {
@@ -11,41 +18,36 @@ function runTest() {
       style: { cssText: "" }
     };
   }
-  function setPrefs(wrap, perLine, spaces, indent) {
-    Firebug.setPref(Firebug.prefDomain, "fireformatCssFormatter.wrapSize", wrap);
-    Firebug.setPref(Firebug.prefDomain, "fireformatCssFormatter.selectorText.selectorsPerLine", perLine);
-    Firebug.setPref(Firebug.prefDomain, "fireformatCssFormatter.selectorText.spaceCount", spaces);
-    Firebug.setPref(Firebug.prefDomain, "fireformatCssFormatter.selectorText.indentLevel", indent); 
-  }
   var formatter = Format.Formatters.getFormatter("com.incaseofstairs.fireformatCSSFormatter");
   
-  // TODO : Tests
   // Formatting with a single token
   FBTest.compare(".single {\n}\n", formatter.format(getStyle(".single")), "Single Token");
   
   // Formatting with multiple tokens, no wrap
-  setPrefs(80, 3, 1, 0);
+  prefs.setPrefs(80, 3, 1, 0);
   FBTest.compare(".single, .double, .triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, no wrap");
 
   // Formatting with multiple tokens, token wrap, no indent
-  setPrefs(80, 2, 1, 0);
+  prefs.setPrefs(80, 2, 1, 0);
   FBTest.compare(".single, .double,\n.triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, token wrap, no indent");
 
   // Formatting with multiple tokens, token wrap, indent
-  setPrefs(80, 2, 2, 1);
+  prefs.setPrefs(80, 2, 2, 1);
   FBTest.compare(".single,  .double,\n  .triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, token wrap, indent");
 
   // Formatting with multiple tokens, char wrap, no indent
-  setPrefs(10, 3, 1, 0);
+  prefs.setPrefs(10, 3, 1, 0);
   FBTest.compare(".single, .double,\n.triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, char wrap, no indent");
 
   // Formatting with multiple tokens, char wrap, indent
-  setPrefs(11, 3, 2, 1);
+  prefs.setPrefs(11, 3, 2, 1);
   FBTest.compare(".single,  .double,\n  .triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, char wrap, indent");
 
   // Formatting with advanced selectors, char wrap, indent
-  setPrefs(10, 3, 2, 1);
+  prefs.setPrefs(10, 3, 2, 1);
   FBTest.compare(".single[name] > DIV.test + *,\n  .double:focus,\n  .triple#myid {\n}\n", formatter.format(getStyle(".single[name] > DIV.test + *, .double:focus, .triple#myid")), "Multiple no wrap");
-  
+
+  prefs.reset();
+
   FBTest.testDone();
 }
