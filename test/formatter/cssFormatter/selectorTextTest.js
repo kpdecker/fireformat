59dbf857d42bd5df530cce056faf9997fc175ee9
kpdecker <kpdecker@gmail.com>
2009-11-28 21:30:04 +0000

    Exclude block data from the selector test.
    git-svn-id: http://fbug.googlecode.com/svn/extensions/fireformat/branches/fireformat1.0@5031 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- test/formatter/cssFormatter/selectorTextTest.js
+++ test/formatter/cssFormatter/selectorTextTest.js
@@ -1,8 +1,5 @@
 /* See license.txt for terms of usage */
 function runTest() {
-  var Format = {};
-  Components.utils.import("resource://fireformat/formatters.jsm", Format);
-
   FBTest.loadScript("FBTestFireformat.js", this);
   var prefs = new FBTestFireformat.PrefHandler([
        "fireformatCssFormatter.wrapSize",
@@ -11,47 +8,48 @@ function runTest() {
        "fireformatCssFormatter.selectorText.indentLevel"
   ]);
 
+  var formatter = Format.Formatters.getFormatter("com.incaseofstairs.fireformatCSSFormatter");
   function getStyle(selectorText) {
-    return {
+    var style = {
       type: CSSRule.STYLE_RULE,
       selectorText: selectorText,
       style: { cssText: "" }
     };
+    return formatter.format(style).replace(/\s*\{[\S\s]*?\}[\S\s]*/, "");
   }
-  var formatter = Format.Formatters.getFormatter("com.incaseofstairs.fireformatCSSFormatter");
   
   // Formatting with a single token
-  FBTest.compare(".single {\n}\n", formatter.format(getStyle(".single")), "Single Token");
+  FBTest.compare(".single", getStyle(".single"), "Single Token");
   
   // Formatting with multiple tokens, no wrap
   prefs.setPrefs(80, 3, 1, 0);
-  FBTest.compare(".single, .double, .triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, no wrap");
+  FBTest.compare(".single, .double, .triple", getStyle(".single, .double, .triple"), "Formatting with multiple tokens, no wrap");
 
   // Formatting with multiple tokens, token wrap, no indent
   prefs.setPrefs(80, 2, 1, 0);
-  FBTest.compare(".single, .double,\n.triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, token wrap, no indent");
+  FBTest.compare(".single, .double,\n.triple", getStyle(".single, .double, .triple"), "Formatting with multiple tokens, token wrap, no indent");
 
   // Formatting with multiple tokens, token wrap, indent
   prefs.setPrefs(80, 2, 2, 1);
-  FBTest.compare(".single,  .double,\n  .triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, token wrap, indent");
+  FBTest.compare(".single,  .double,\n  .triple", getStyle(".single, .double, .triple"), "Formatting with multiple tokens, token wrap, indent");
 
   // Formatting with multiple tokens, char wrap, no indent
   prefs.setPrefs(10, 3, 1, 0);
-  FBTest.compare(".single, .double,\n.triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, char wrap, no indent");
+  FBTest.compare(".single, .double,\n.triple", getStyle(".single, .double, .triple"), "Formatting with multiple tokens, char wrap, no indent");
 
   // Formatting with multiple tokens, char wrap, indent
   prefs.setPrefs(11, 3, 2, 1);
-  FBTest.compare(".single,  .double,\n  .triple {\n}\n", formatter.format(getStyle(".single, .double, .triple")), "Formatting with multiple tokens, char wrap, indent");
+  FBTest.compare(".single,  .double,\n  .triple", getStyle(".single, .double, .triple"), "Formatting with multiple tokens, char wrap, indent");
 
   // Formatting with advanced selectors, char wrap, indent
   prefs.setPrefs(10, 3, 2, 1);
-  FBTest.compare(".single[name] > DIV.test + *,\n  .double:focus,\n  .triple#myid {\n}\n", formatter.format(getStyle(".single[name] > DIV.test + *, .double:focus, .triple#myid")), "Multiple no wrap");
+  FBTest.compare(".single[name] > DIV.test + *,\n  .double:focus,\n  .triple#myid", getStyle(".single[name] > DIV.test + *, .double:focus, .triple#myid"), "Multiple no wrap");
 
 
   // Formatting with comma strings in the selector
   prefs.setPrefs(10, 1, 2, 1);
-  FBTest.compare(".single[test=\"fail, fail\"] > DIV.test + *,\n  .double:focus,\n  .triple#myid {\n}\n", formatter.format(getStyle(".single[test=\"fail, fail\"] > DIV.test + *, .double:focus, .triple#myid")), "Comma selector string 1");
-  FBTest.compare(".single[test=\'fail, fail\'] > DIV.test + *,\n  .double:focus,\n  .triple#myid {\n}\n", formatter.format(getStyle(".single[test=\'fail, fail\'] > DIV.test + *, .double:focus, .triple#myid")), "Comma selector string 2");
+  FBTest.compare(".single[test=\"fail, fail\"] > DIV.test + *,\n  .double:focus,\n  .triple#myid", getStyle(".single[test=\"fail, fail\"] > DIV.test + *, .double:focus, .triple#myid"), "Comma selector string 1");
+  FBTest.compare(".single[test=\'fail, fail\'] > DIV.test + *,\n  .double:focus,\n  .triple#myid", getStyle(".single[test=\'fail, fail\'] > DIV.test + *, .double:focus, .triple#myid"), "Comma selector string 2");
   
   prefs.reset();
 
