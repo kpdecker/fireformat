d68ebc1527d1fbddccf8ba2ff4b23ec685c81b18
kpdecker <kpdecker@gmail.com>
2009-12-02 00:43:53 +0000

    Add setGlobal and setPref vararg support to the test preference handler
    git-svn-id: http://fbug.googlecode.com/svn/extensions/fireformat/branches/fireformat1.0@5079 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- test/FBTestFireformat.js
+++ test/FBTestFireformat.js
@@ -6,20 +6,32 @@ var Firebug = FW.Firebug;
 
 var FBTestFireformat = {
     PrefHandler: function(prefs) {
-      var Firebug = FW.Firebug;
-      var original = [];
+      var original = [], globals = {};
       for (var i = 0; i < prefs.length; i++) {
         original.push(Firebug.getPref(Firebug.prefDomain, prefs[i]));
       }
       
       return {
+        setGlobal: function(pref, value) {
+          if (!globals.hasOwnProperty(pref)) {
+            globals[pref] = Firebug.getPref(Firebug.prefDomain, pref);
+          }
+          Firebug.setPref(Firebug.prefDomain, pref, value);
+        },
         setPrefs: function() {
-          for (var i = 0; i < prefs.length; i++) {
+          for (var i = 0; i < arguments.length; i++) {
             Firebug.setPref(Firebug.prefDomain, prefs[i], arguments[i]);
           }
         },
         reset: function() {
           this.setPrefs.apply(this, original);
+
+          for (var x in globals) {
+            if (globals.hasOwnProperty(x)) {
+              Firebug.setPref(Firebug.prefDomain, x, globals[x]);
+            }
+          }
+          globals = {};
         }
       };
     }
