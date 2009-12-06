d7e77079f5045bd37cf64fd9736816dd675978d5
kpdecker <kpdecker@gmail.com>
2009-12-06 03:05:50 +0000

    Update HTML formatter for the new writer interface and fill in support for some missing Node types.
    git-svn-id: http://fbug.googlecode.com/svn/extensions/fireformat/branches/fireformat1.0@5151 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- content/fireformat/formatters/fireformatHtmlFormatter.js
+++ content/fireformat/formatters/fireformatHtmlFormatter.js
@@ -7,22 +7,60 @@ FBL.ns(function() { with (FBL) {
   };
   DOMFormatter.prototype = {
     printNode: function(node) {
-      if (node instanceof Element) {
+      var type = node.nodeType;
+
+      if (type == Node.ELEMENT_NODE) {
         this.printElement(node);
-      } else if (node instanceof Text) {
+      } else if (type == Node.ATTRIBUTE_NODE) {
+        this.printAttr(node);
+      } else if (type == Node.TEXT_NODE) {
         this.printText(node);
+      } else if (type == Node.CDATA_SECTION_NODE) {
+        this.printCDATA(node);
+      // Node.ENTITY_REFRENCE_NODE
+      // Node.ENTITY_NODE
+      // Node.PROCESSING_INSTRUCTION_NODE
+      } else if (type == Node.COMMENT_NODE) {
+        this.printComment(node);
+      } else if (type == Node.DOCUMENT_NODE) {
+        this.printDocument(node);
+      } else if (type == Node.DOCUMENT_TYPE_NODE) {
+        this.printDocType(node);
+      } else {
+        // TODO : Remove
+        FBTrace.sysout(node + " " + node.nodeType, node.wrappedJSObject || node);
       }
+      // Node.DOCUMENT_FRAGMENT_NODE
+      // Node.NOTATION_NODE
     },
     
+    printDocument: function(doc) {
+      var child = doc.firstChild;
+      while (child) {
+        this.printNode(child);
+        child = child.nextSibling;
+      }
+    },
+    printDocType: function(docType) {
+      // TODO : Check this against the spec and examine the formatting options that are available
+      this.writer.write({ value: "<!DOCTYPE", join: " " });
+      this.writer.write({ value: docType.name.toLowerCase(), join: " " });
+      this.writer.write({ value: "PUBLIC", join: " " });
+      this.writer.write({ value: "\"" + docType.publicId + "\"", join: " " });
+      this.writer.write("\"" + docType.systemId + "\"");
+      this.writer.write({ value: ">", join: "\n", nowrap: true });
+    },
     printElement: function(el) {
+      var tagName = el.tagName;    // TODO : Update this for XHTML and HTML pref
+      var childNodes = el.childNodes;
+
       this.writer.write("<");
-      this.writer.write(el.tagName);
+      this.writer.write(tagName);
       
       var attrs = el.attributes;
       for (var i = 0; i < attrs.length; i++) {
         this.printAttr(attrs[i]);
       }
-      this.writer.write(">");
 
       var childNodes = el.childNodes;
       for (var i = 0; i < childNodes.length; i++) {
@@ -60,8 +98,9 @@ FBL.ns(function() { with (FBL) {
     name: "com.incaseofstairs.fireformatHTMLFormatter",
     display: i18n.getString("FireformatHTMLFormatter"),
     format: function(object) {
-      var writer = new Fireformat.Writer("  ");
-      new DOMFormatter(writer).printNode(object);
+      var prefCache = new Fireformat.PrefCache("extensions.firebug.fireformatHtmlFormatter"),
+          writer = new Fireformat.Writer(prefCache);
+      new DOMFormatter(writer, prefCache).printNode(object);
       return writer.toString();
     }
   });
---
+++ defaults/preferences/htmlFormatter.js
@@ -0,0 +1,8 @@
+/* See license.txt for terms of usage */
+
+/*
+ * Formatter-wide preferences
+ */
+pref("extensions.firebug.fireformatHtmlFormatter.wrapSize", 80);    // Only affects attribute lists
+pref("extensions.firebug.fireformatHtmlFormatter.indentChar", ' ');
+pref("extensions.firebug.fireformatHtmlFormatter.indentCount", 2);
