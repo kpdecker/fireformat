a1fbc0121df2560b7a4613ffa0d020f8ce217a28
kpdecker <kpdecker@gmail.com>
2009-12-29 03:41:21 +0000

    Move show whitespace functionality into the format test control. Better sizing of the format test control.
    git-svn-id: http://fbug.googlecode.com/svn/extensions/fireformat/branches/fireformat1.0@5467 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,3 +1,5 @@
 /* See license.txt for terms of usage */
 pref("extensions.firebug.fireformat.htmlFormatter", "com.incaseofstairs.fireformatHTMLFormatter");
 pref("extensions.firebug.fireformat.cssFormatter", "com.incaseofstairs.fireformatCSSFormatter");
+
+pref("extensions.firebug.fireformat.preview.showWhitespace", false);
