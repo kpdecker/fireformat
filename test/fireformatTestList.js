51f100525dfb971f62a4f9e6390c7321f10a5b2e
kpdecker <kpdecker@gmail.com>
2010-01-18 21:46:53 +0000

    Update firediff and fireformat tests to allow merged UTs.
    git-svn-id: http://fbug.googlecode.com/svn/extensions/fireformat/branches/fireformat1.0@5735 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ test/fireformatTestList.js
@@ -0,0 +1,17 @@
+﻿var testList = testList || [];
+
+testList.push.apply(testList, [
+                {group: "formatter",    uri: "formatter/wrapTokensTest.js",                         desc: "Formatter: Wrap Tokens Test" },
+                {group: "formatter",    uri: "formatter/writerTest.js",                             desc: "Formatter: Writer Test" },
+                {group: "cssFormatter", uri: "formatter/cssFormatter/selectorTextTest.js",          desc: "CSS Formatter: Selector Text Test" },
+                {group: "cssFormatter", uri: "formatter/cssFormatter/blockTextTest.js",             desc: "CSS Formatter: Block Text Test" },
+                {group: "cssFormatter", uri: "formatter/cssFormatter/propertyTextTest.js",          desc: "CSS Formatter: Property Text Test" },
+                {group: "cssFormatter", uri: "formatter/cssFormatter/atRuleTextTest.js",            desc: "CSS Formatter: Simple @Rule Text Test" },
+                {group: "cssFormatter", uri: "formatter/cssFormatter/cssFormatterTest.js",          desc: "CSS Formatter: CSS Formatter Test" },
+                {group: "htmlFormatter", uri: "formatter/htmlFormatter/entityTextTest.js",          desc: "HTML Formatter: Entity Text Test" },
+                {group: "htmlFormatter", uri: "formatter/htmlFormatter/textWrapTest.js",            desc: "HTML Formatter: Text Wrap Test" },
+                {group: "htmlFormatter", uri: "formatter/htmlFormatter/elementTextTest.js",         desc: "HTML Formatter: Element Text Test" },
+                {group: "htmlFormatter", uri: "formatter/htmlFormatter/attributeTextTest.js",       desc: "HTML Formatter: Attribute Text Test" },
+                {group: "htmlFormatter", uri: "formatter/htmlFormatter/htmlFormatterTest.js",       desc: "HTML Formatter: HTML Formatter Test" },
+                {group: "htmlFormatter", uri: "formatter/htmlFormatter/xhtmlFormatterTest.js",      desc: "HTML Formatter: XHTML Formatter Test" }
+]);
